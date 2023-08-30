8801f7fa0eb260a2e38ba604aa4418243902c4bf
agvan <agvandyke25@gmail.com>
2023-08-29 22:01:22 -0500

    created default export for global css

---
+++ nextjs-blog/pages/_app.js
@@ -0,0 +1,5 @@
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+  }
+  // default expot of _app.js is a top level 
+  // React component that wraps all the pages in the app
