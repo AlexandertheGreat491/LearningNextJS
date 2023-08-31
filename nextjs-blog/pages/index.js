d8bc7bde8db831e5744a1b2d0d589dbacd3b1d90
agvan <agvandyke25@gmail.com>
2023-08-31 11:19:02 -0500

    added function

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -3,6 +3,14 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import {getSortedPostsData} from '../lib/posts';
 
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
 
 export default function Home() {
   return (
