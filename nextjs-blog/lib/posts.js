3601c6d1ea9db037b970e2a0d35152a0c8ab84dc
agvan <agvandyke25@gmail.com>
2023-09-05 19:35:41 -0500

    added getPostData(id) function & one comment

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -58,3 +58,17 @@ export function getAllPostIds() {
     };
   });
 }
+// returns the post based on id
+export function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Combine the data with the id
+  return {
+    id,
+    ...matterResult.data,
+  };
+}
