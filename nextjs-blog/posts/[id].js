2b4348d212d9719645e542a0cda86f53c1467fdd
agvan <agvandyke25@gmail.com>
2023-09-05 19:42:08 -0500

    removed unnecessary code

--- nextjs-blog/posts/[id].js
+++ nextjs-blog/posts/[id].js
@@ -24,9 +24,7 @@ export default function Post({ postData }) {
 }
 
 
-//export default function Post() {
-  //return <Layout>...</Layout>;
-//}
+
 
 // post page now using the getPostData function in getStaticProps to get
 // post data & return it as props
